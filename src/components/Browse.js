fdf0d01d21a9afb704a7940a2e38741a483d2572
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-05-13 15:00:40 +0530

    created movie slice

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
 import { api_options, api_url } from "../utils/constants";
+import { useDispatch } from "react-redux";
+import { addNowPlayingMovie } from "../redux/MovieSlice";
 
 const Browse = () => {
+  const dispatch = useDispatch();
+
   const getShowByFilter = async () => {
     const response = await fetch(api_url, api_options);
     const data = await response.json();
     console.log(data.shows);
+    dispatch(addNowPlayingMovie(data.shows));
   };
 
   useEffect(() => {
--- src/redux/AppStore.js
+++ src/redux/AppStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import movieReducer from "./MovieSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movies: movieReducer,
   },
 });
